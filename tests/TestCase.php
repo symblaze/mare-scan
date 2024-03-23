f3541b9dfb8fa180c8b90d2b0e0b40a03a3ad64c
imdhemy <imdhemy@gmail.com>
2024-03-23 14:36:05 +0200

    chore(tests): add fixture path helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,9 +13,17 @@ abstract class TestCase extends PHPUnitTestCase
 {
     protected function fixture(string $path): string
     {
-        $contents = file_get_contents(__DIR__.'/fixtures/'.$path);
+        $contents = file_get_contents($this->fixturePath($path));
         assert(is_string($contents), 'Fixture not found.');
 
         return $contents;
+    }
+
+    protected function fixturePath(string $path): string
+    {
+        $realpath = realpath(__DIR__.'/fixtures/'.$path);
+        assert(is_string($realpath), 'Fixture not found.');
+
+        return $realpath;
     }
 }
