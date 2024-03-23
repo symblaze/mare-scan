0be9b18cb67c67be3739cebc301af0e53277ebb0
imdhemy <imdhemy@gmail.com>
2024-03-23 13:06:24 +0200

    chore: add return data type

--- src/Parser/ParserInterface.php
+++ src/Parser/ParserInterface.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Symblaze\MareScan\Parser;
 
+use PhpParser\Node\Stmt;
+
 interface ParserInterface
 {
     public function targetVersion(): string;
 
+    /**
+     * @return Stmt[]
+     */
     public function parse(string $code): array;
 }
