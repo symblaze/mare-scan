e21fcdb9d38250254533fc2b1e66f0307c45cc88
imdhemy <imdhemy@gmail.com>
2024-07-17 07:55:04 +0200

    chore: improve scanner design
    - Fixes #14

--- src/Scanner/Scanner.php
+++ src/Scanner/Scanner.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Symblaze\MareScan\Scanner;
 
+use Closure;
 use SplFileInfo;
 use Symblaze\MareScan\Config\Config;
+use Symblaze\MareScan\Inspector\CodeIssue;
 
 final readonly class Scanner implements ScannerInterface
 {
@@ -16,6 +18,17 @@ final readonly class Scanner implements ScannerInterface
     ) {
     }
 
+    public static function create(Config $config, ?Closure $callback = null): self
+    {
+        $fileScanner = new FileScanner($config->getParser(), $callback);
+        $multipleFileScanner = new MultipleFileScanner($fileScanner);
+
+        return new self($config, $multipleFileScanner);
+    }
+
+    /**
+     * @return array<string, CodeIssue[]>
+     */
     public function scan(?array $inspectors = null, ?SplFileInfo $file = null): array
     {
         return $this->scanner->scan($this->config->getInspectors(), ...$this->config->getFinder()->getIterator());
